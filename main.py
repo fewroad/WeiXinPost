03b9b23044e6adbfc1cd0714d93f31dacc3c95ce
fewroad <127120711+fewroad@users.noreply.github.com>
2023-12-14 17:28:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -71,13 +71,13 @@ def get_Week_Classes(w):
 
 
 # 获取今日课程
-#def get_Today_Class():
-#    year = localtime().tm_year
-#    month = localtime().tm_mon
-#    day = localtime().tm_mday
-#   today = datetime.date(datetime(year=year, month=month, day=day))
-#    todayClasses = get_Week_Classes(None)[today.weekday()]
-#   return todayClasses
+def get_Today_Class():
+    year = localtime().tm_year
+    month = localtime().tm_mon
+    day = localtime().tm_mday
+    today = datetime.date(datetime(year=year, month=month, day=day))
+    todayClasses = get_Week_Classes(None)[today.weekday()]
+   return todayClasses
 
 
 # 获取指定星期几的课程
