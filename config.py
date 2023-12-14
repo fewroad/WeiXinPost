344f6b6b4d1524a0443ed6e4dd3f0923157a9f3e
fewroad <127120711+fewroad@users.noreply.github.com>
2023-12-14 15:49:47 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
 # 公众号配置
 # 公众号appId
-app_id = "wx******************"
+app_id = "wx3fdfe0e286fc8741"
 # 公众号appSecret
-app_secret = "f9a80260d33******************"
+app_secret = "1306b872df510b960705d5fefeda7f19"
 # 模板消息id
 # 每日消息
-template_id1 = "wX-************************************"
+template_id1 = "lBaoQVry6gjkjPP8FHaywfwtlwmkB-kdR9rBQ5XD6fc"
 # 课程消息,上课提醒
-template_id2 = "loe1yHWr************************************"
+template_id2 = "80enWK6k0f_THdy1JHcsIwqiPCGzEHVmSvCUT4B481E"
 # 晚安心语
-template_id3 = "-fun9-2************************************"
+template_id3 = "ExIk5HZK8nPT9vhbhSwpzr0USk7k7QfnSKDYDWQfBjE"
 # 接收公众号消息的微信号
 # 这是openid
-user = ["ojhdR50T******************"]
+user = ["o29V-6G3qXqew848F8jfxvEF1T64"]
 
 # 信息配置
 # 所在省份
